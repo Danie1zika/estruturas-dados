34e4edd779be9b86d7811a0ca2e7d9840afa87f4
Danie1zika <danielbrandao03420@gmail.com>
2024-05-06 10:43:52 -0400

    comando findIndex

--- array/findIndex.js
+++ array/findIndex.js
@@ -1,6 +1,6 @@
 let array = [1, 2, 3, 4, 5];
 let index = array.findIndex(function(element) {
-    return element > 3.
+    return element > 3;
 });
 console.log(index);
 // Saída: 3 (indice do primeiro elemento maior que 3)
