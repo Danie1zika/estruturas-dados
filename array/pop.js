c4f25d607e421c15f442bf644a18d6dd9270c3b8
Danie1zika <danielbrandao03420@gmail.com>
2024-05-06 08:58:32 -0400

    comando pop

--- array/pop.js
+++ array/pop.js
@@ -1,4 +1,4 @@
 let array = [1, 2, 3];
-let removeElement = array.pop();
-console.log(removeElement); // Saída: 3
+let removedElement = array.pop();
+console.log(removedElement); // Saída: 3
 console.log(array); // Saída: [1, 2]
