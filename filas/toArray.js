9ebb044356041fbb7fe0d7563a844c077267d16e
Danie1zika <danielbrandao03420@gmail.com>
2024-05-13 09:30:17 -0400

    toArray

--- filas/toArray.js
+++ filas/toArray.js
@@ -2,5 +2,5 @@ let fila = [1, 2, 3];
 fila.toArray = function() {
     return this.slice();
 };
-let arraFila = fila.toArray();
+let arrayFila = fila.toArray();
 console.log(arrayFila); // Saída: [1, 2, 3]
