ddb64b73b5f25b04070b801f9179e1d02bcf091b
Danie1zika <danielbrandao03420@gmail.com>
2024-05-13 09:35:35 -0400

    toString

--- filas/toString.js
+++ filas/toString.js
@@ -2,4 +2,4 @@ let fila = [1, 2, 3];
 fila.toString = function() {
     return thisjoin(', ');
 };
-console.log(ifla.toString()); // Saída: "1, 2, 3"
+console.log(fila.toString()); // Saída: "1, 2, 3"
